1e8ba71aa7484eeaada414bb67a0b71aaf080ee3
FaroukZeino <33427443+FaroukZeino@users.noreply.github.com>
2019-05-17 21:10:46 +0300

    Initial Commit

---
+++ Delegate Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate Example
+//
+//  Created by FarouK on 17/05/2019.
+//  Copyright © 2019 FarouK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
